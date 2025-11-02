d557e136ff6b3782f2c39d5e9a6e90330aee2a18
Diego Montalti <diego.montalti@studio.unibo.it>
2025-11-02 17:45:56 +0100

    Completing BFS algorithm for method getPath

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,10 +1,13 @@
 package it.unibo.generics.graph.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
@@ -21,12 +24,14 @@ public class GraphImpl<N> implements Graph<N> {
     }
 
     public void addEdge(N source, N target) {
-        if(!mapForGraph.containsKey(source)) {
-            mapForGraph.put(source, new ArrayList<>());
-        }
-        List<N> tempList = mapForGraph.get(source);
-        if(!tempList.contains(target)) {
-            tempList.add(target);
+        if (source!=null && target!=null) {
+            if(!mapForGraph.containsKey(source)) {
+                mapForGraph.put(source, new ArrayList<>());
+            }
+            List<N> tempList = mapForGraph.get(source);
+            if(!tempList.contains(target)) {
+                tempList.add(target);
+            }
         }
     }
 
@@ -42,10 +47,39 @@ public class GraphImpl<N> implements Graph<N> {
         return new HashSet<>();
     }
 
-    @Override
     public List<N> getPath(N source, N target) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+        if(!mapForGraph.containsKey(source) || !mapForGraph.containsKey(target)) {
+            return null;
+        }
+
+        Queue<N> queue = new LinkedList<>();
+        Map<N,N> parent = new HashMap<>();
+        Set<N> visited = new HashSet<>();
+
+        queue.add(source);
+        visited.add(source);
+
+        while(!queue.isEmpty()) {
+            N current = queue.poll();
+
+            if(current.equals(target)) {
+                List<N> path = new ArrayList<>();
+                for(N solution = target; solution != null; solution = parent.get(solution)) {
+                    path.add(solution);
+                }
+                Collections.reverse(path);
+                return path;
+            }
+
+            for(N neighbor : mapForGraph.get(current)) {
+                if(!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    parent.put(neighbor,current);
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return null;
     }
 
 }
