a7ea0f020cd8580c93caa62b55dd0ed9f4358682
Diego Montalti <diego.montalti@studio.unibo.it>
2025-11-02 14:37:04 +0100

    Changing permission on gradlew and fixing an error on ServiceBehindUnstableNetwork: i've put IllegalStateException where IllegalArgumentException was expected. Exercises completed

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -61,7 +61,7 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         } else {
             final var message = data + " is not a valid keyword (allowed: " + KEYWORDS + "), nor is a number";
             commandQueue.clear();
-            throw new IllegalStateException(message, exceptionWhenParsedAsNumber);
+            throw new IllegalArgumentException(message, exceptionWhenParsedAsNumber);
             /*
              * This method, in this point, should throw an IllegalStateException.
              * Its cause, however, is the previous NumberFormatException.
